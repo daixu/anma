d7d96064dc682eda8e67fc8b1d896936958a4263
speedystone <googler_vip@qq.com>
2020-01-16 10:27:31 +0800

    update code

--- src/components/my/index.js
+++ src/components/my/index.js
@@ -11,19 +11,22 @@ import icon_sign from '../../static/icon_sign.png';
 import icon_message from '../../static/icon_message.png';
 import icon_service from '../../static/icon_service.png';
 import icon_welfare from '../../static/icon_welfare.png';
+import icon_invitation_code from '../../static/icon_invitation_code.png';
+import icon_author_my from '../../static/icon_author_my.png';
+import icon_help from '../../static/icon_help.png';
 import {Link} from "react-router-dom";
 
 class My extends React.Component {
 
     render() {
         return (
-            <div>
-                <div>
-                    <div className="nav-cont">
-                        <span className="nav-text">我的</span>
-                    </div>
+            <div className="my-content-div">
+                <div className="nav-cont">
+                    <span className="nav-text">我的</span>
+                </div>
 
-                    <div className="my-user-top">
+                <div className="my-user-top">
+                    <div className="my-user-info-top">
                         <img className="my-user-avatar" src={default_avatar} alt="user-avatar"/>
                         <div className="my-user-info">
                             <p className="my-username">{this.props.dataUserInfo.get('nickname') || '点击登录'}</p>
@@ -42,36 +45,68 @@ class My extends React.Component {
                             <button className="my-user-recharge-btn">充值</button>
                         </div>
                     </div>
+                </div>
 
-                    <div className="my-option-1">
-                        <div className="my-option-content">
-                            <ul className="my-option-list page current">
-                                <li className="option-content">
-                                    <Link to={'/detail/'} className="strong-list-a">
-                                        <img src={icon_sign} className="my-option-img-a" alt=""/>
-                                        <p className="story-name">签到</p>
-                                    </Link>
-                                </li>
-                                <li className="option-content">
-                                    <Link to={'/detail/'} className="strong-list-a">
-                                        <img src={icon_welfare} className="my-option-img-a" alt=""/>
-                                        <p className="story-name">福利</p>
-                                    </Link>
-                                </li>
-                                <li className="option-content">
-                                    <Link to={'/detail/'} className="strong-list-a">
-                                        <img src={icon_message} className="my-option-img-a" alt=""/>
-                                        <p className="story-name">消息</p>
-                                    </Link>
-                                </li>
-                                <li className="option-content">
-                                    <Link to={'/detail/'} className="strong-list-a">
-                                        <img src={icon_service} className="my-option-img-b" alt=""/>
-                                        <p className="story-name">客服</p>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
+                <div className="my-option">
+                    <div className="my-option-content">
+                        <ul className="my-option-list page current">
+                            <li className="option-content">
+                                <Link to={'/detail/'} className="strong-list-a">
+                                    <img src={icon_sign} className="my-option-img-a" alt=""/>
+                                    <p className="story-name">签到</p>
+                                </Link>
+                            </li>
+                            <li className="option-content">
+                                <Link to={'/detail/'} className="strong-list-a">
+                                    <img src={icon_welfare} className="my-option-img-a" alt=""/>
+                                    <p className="story-name">福利</p>
+                                </Link>
+                            </li>
+                            <li className="option-content">
+                                <Link to={'/detail/'} className="strong-list-a">
+                                    <img src={icon_message} className="my-option-img-a" alt=""/>
+                                    <p className="story-name">消息</p>
+                                </Link>
+                            </li>
+                            <li className="option-content">
+                                <Link to={'/detail/'} className="strong-list-a">
+                                    <img src={icon_service} className="my-option-img-b" alt=""/>
+                                    <p className="story-name">客服</p>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="my-option-content">
+                        <ul className="my-option-list page1 current">
+                            <li className="option-content-ver">
+                                <img src={icon_invitation_code} alt="next" className="my-option-img-c"/>
+                                <span className="option-content-title">
+                                    邀请码
+                                </span>
+                                <span className="my-arrow">
+                                    <img src={arrow} alt="next"/>
+                                </span>
+                            </li>
+                            <li className="option-content-ver">
+                                <img src={icon_author_my} alt="next" className="my-option-img-d"/>
+                                <span className="option-content-title">
+                                    作者专区
+                                </span>
+                                <span className="my-arrow">
+                                    <img src={arrow} alt="next"/>
+                                </span>
+                            </li>
+                            <li className="option-content-ver-bottom">
+                                <img src={icon_help} alt="next" className="my-option-img-e"/>
+                                <span className="option-content-title">
+                                    帮助
+                                </span>
+                                <span className="my-arrow">
+                                    <img src={arrow} alt="next"/>
+                                </span>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
