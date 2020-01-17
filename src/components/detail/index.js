03a91d1efe47977eb11e0b1aa66e0a07c00f1c1f
speedystone <googler_vip@qq.com>
2020-01-17 15:52:57 +0800

    update code

--- src/components/detail/index.js
+++ src/components/detail/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './index.css';
 import default_book_cover from './../../static/default_bookcover.jpg';
-import {Button, ActivityIndicator} from "antd-mobile";
+import {ActivityIndicator, Button} from "antd-mobile";
 import img_back from './../../static/img_back.png';
 import {Link} from "react-router-dom";
 import arrow from "../../static/arrow.png";
@@ -65,7 +65,7 @@ class Detail extends React.Component {
             const data = response.data;
             this.setState({
                 sumreplycount: data.data.sumreplycount,
-                comdata:data.data.comdata,
+                comdata: data.data.comdata,
                 animating: false
             })
         }).catch(() => {
@@ -76,7 +76,7 @@ class Detail extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            detailsdata:{},
+            detailsdata: {},
             likedata: [],
             sumreplycount: 0,
             comdata: [],
@@ -87,84 +87,85 @@ class Detail extends React.Component {
     render() {
         const {detailsdata, likedata, comdata, sumreplycount, animating} = this.state;
         return (
-            <div>
-                <div className="nav-cont">
+            animating === true ?
+                    <ActivityIndicator
+                        toast
+                        text="数据加载中..."
+                        animating={animating}
+                    /> :
+                <div>
+                    <div className="nav-cont">
                         <span className="nav-back" onClick={() => this.goBack()}>
                             <img src={img_back} alt="back"/>
                         </span>
-                    <span className="nav-text">书籍详情</span>
-                </div>
-
-                <ActivityIndicator
-                    toast
-                    text="数据加载中..."
-                    animating={animating}
-                />
-
-                <div className="book-detail-info">
-                    <div className="book-img">
-                        <img src={detailsdata.bookcover || default_book_cover} alt=""
-                             className="book-cover"/>
-                        <div className="book-info">
-                            <p className="book-title">{detailsdata.bookname}</p>
-                            <p className="book-author">作者: {detailsdata.author}</p>
-                            <p className="book-author">类别: {detailsdata.classname}</p>
-                            <p className="book-author">字数: {detailsdata.wordnumbers} | {detailsdata.serstatus}</p>
-                            <p className="book-author">点击: {detailsdata.clicknumber}</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="book-detail-content">
-                    <div className="detail-button">
-                        <Button type="warning" inline style={{width: '48%'}} onClick={this.startReading.bind(this)}>开始阅读</Button>
-                        <Button type="primary" inline style={{
-                            width: '48%',
-                            float: 'right',
-                            background: '#f5f5f5',
-                            color: '#2d2d2d'
-                        }} onClick={() => this.login()}>加入书架</Button>
+                        <span className="nav-text">书籍详情</span>
                     </div>
 
-                    <div className="book-synopsis">
-                        <div className="detail-book-synopsis">
-                            <div className="detail-book-synopsis-container">
-                                <div className="detail-book-synopsis-content">内容简介:{detailsdata.synopsis}</div>
+                    <div className="book-detail-info">
+                        <div className="book-img">
+                            <img src={detailsdata.bookcover || default_book_cover} alt=""
+                                 className="book-cover"/>
+                            <div className="book-info">
+                                <p className="book-title">{detailsdata.bookname}</p>
+                                <p className="book-author">作者: {detailsdata.author}</p>
+                                <p className="book-author">类别: {detailsdata.classname}</p>
+                                <p className="book-author">字数: {detailsdata.wordnumbers} | {detailsdata.serstatus}</p>
+                                <p className="book-author">点击: {detailsdata.clicknumber}</p>
                             </div>
                         </div>
                     </div>
 
-                    <div className="book-detail-catalog">
-                        <Link to={'/directory/' + detailsdata.bookid} className="book-detail-catalog-a">
-                            <span>目录</span>
-                            <span className="book-detail-arrow">
+                    <div className="book-detail-content">
+                        <div className="detail-button">
+                            <Button type="warning" inline style={{width: '48%'}}
+                                    onClick={this.startReading.bind(this)}>开始阅读</Button>
+                            <Button type="primary" inline style={{
+                                width: '48%',
+                                float: 'right',
+                                background: '#f5f5f5',
+                                color: '#2d2d2d'
+                            }} onClick={() => this.login()}>加入书架</Button>
+                        </div>
+
+                        <div className="book-synopsis">
+                            <div className="detail-book-synopsis">
+                                <div className="detail-book-synopsis-container">
+                                    <div className="detail-book-synopsis-content">内容简介:{detailsdata.synopsis}</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="book-detail-catalog">
+                            <Link to={'/directory/' + detailsdata.bookid} className="book-detail-catalog-a">
+                                <span>目录</span>
+                                <span className="book-detail-arrow">
                                 <img src={arrow} alt="next"/>
                             </span>
-                        </Link>
-                    </div>
+                            </Link>
+                        </div>
 
-                    <div className="book-detail-middle">
-                        <Link className="book-detail-comment" to={'/index'}>
-                            <span className="book-detail-comment-header">书评区</span>
-                            <span className="book-detail-comment-number">{sumreplycount}人评论</span>
-                            <span className="book-detail-arrow">
+                        <div className="book-detail-middle">
+                            <Link className="book-detail-comment" to={'/index'}>
+                                <span className="book-detail-comment-header">书评区</span>
+                                <span className="book-detail-comment-number">{sumreplycount}人评论</span>
+                                <span className="book-detail-arrow">
                                 <img src={arrow} alt="next"/>
                             </span>
-                        </Link>
+                            </Link>
 
-                        <Comment data={comdata}/>
+                            <Comment data={comdata}/>
 
-                        <Link className="book-detail-comment" to={'/index'}>
-                            <span className="book-detail-comment-header">猜你喜欢</span>
-                            <span className="book-detail-arrow">
+                            <Link className="book-detail-comment" to={'/index'}>
+                                <span className="book-detail-comment-header">猜你喜欢</span>
+                                <span className="book-detail-arrow">
                                 <img src={arrow} alt="next"/>
                             </span>
-                        </Link>
+                            </Link>
 
-                        <Like data={likedata}/>
+                            <Like data={likedata}/>
+                        </div>
                     </div>
                 </div>
-            </div>
         )
     }
 
