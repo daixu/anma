d270c0bef4707c24a225c4ea5f48263b6d1ebd40
speedystone <googler_vip@qq.com>
2020-01-19 13:46:22 +0800

    update code

--- src/components/booklist/index.js
+++ src/components/booklist/index.js
@@ -5,6 +5,7 @@ import './../../css/main.css';
 import {connect} from 'react-redux';
 import img_back from "../../static/img_back.png";
 import { loadBookList } from './../../store/actionCreators.js';
+import {Link} from "react-router-dom";
 
 class BookList extends React.Component {
 
@@ -13,7 +14,6 @@ class BookList extends React.Component {
     }
 
     render() {
-        {console.log('data123= ', this.props.dataBookList)}
         return (
             <div>
                 <div className="nav-cont">
@@ -21,6 +21,35 @@ class BookList extends React.Component {
                             <img src={img_back} alt="back"/>
                         </span>
                     <span className="nav-text">书籍列表</span>
+                </div>
+
+                <div className="recommend-list">
+                    <div className="recommend-list-content">
+                        <ul className="recommend-book-ul">
+                            {
+                                this.props.dataBookList.get('pagedata').map(function (item) {
+                                    return (
+                                        <li key={item.get('bookid')} className="recommend-book-li">
+                                            <Link to={'/detail/' + item.get('bookid')}>
+                                                <div className="recommend-book-info">
+                                                    <img src={item.get('bookcover')} className="recommend-book-cover" alt=""/>
+
+                                                    <div className="recommend-book-content">
+                                                        <p className="recommend-book-title">{item.get('bookname')}</p>
+                                                        <span className="recommend-author">{item.get('author')}·{item.get('classname')}·{item.get('serstatus')}·{item.get('wordnumbers')}</span>
+
+                                                        <div className="recommend-book-rand-a">
+                                                            <div className="recommend-book-rand-content">{item.get('synopsis')}</div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </Link>
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
                 </div>
             </div>
         )
